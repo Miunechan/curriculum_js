2c22beff022652f1e818191bd84347dc4d8ae77d
Miunechan <ishida.rmrm@gmail.com>
2023-09-08 11:20:03 +0900

    課題提出_新規1-2

---
+++ JavaScript/1-2/main.js
@@ -0,0 +1,31 @@
+/*
+let name = "hoge";
+//nameと書いたのに、hogeと出力されるはずです
+console.log(name);
+
+name = "fuga";
+console.log(name);
+
+let name = "池田";
+console.log(name + "くん");
+let number = 10;
+console.log(number * 3);
+
+//変数名は「 ${ } 」で囲むことで
+//一つの文字列の中に変数や定数を埋め込む ことができる
+console.log(`${name}くんは${number}月生まれです`);
+
+let name = 'iwasaki';
+console.log(age); //age定義してないのでエラーとなる
+
+let name = 'iwasaki';
+let name = 'tanaka'; //nameは定義済みなので怒られる
+console.log(name);
+*/
+console.log("課題");
+
+let price = 100;
+console.log(price * 2);
+
+price = 500;
+console.log(price * 2);
